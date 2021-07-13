d0af717ddc9bb39eb9cd8714b4b32f687556d686
aasthamanya <54213825+aasthamanya@users.noreply.github.com>
2021-07-13 16:23:07 +0530

    changes

--- EightQueens_Question/src/ChessBoardRenderer.java
+++ EightQueens_Question/src/ChessBoardRenderer.java
@@ -1,7 +1,26 @@
 public class ChessBoardRenderer {
 	public boolean isBlackSquare(int square) {
 
-		// WRITE YOUR LOGIC HERE...
+		int col = (square%8);
+		int row;
+		if(square<8)
+			row= 0;
+		else if(square>=8 && square<16)
+			row= 1;
+		else if(square>=16 && square<24)
+			row= 2;
+		else if(square>=24 && square<32)
+			row= 3;
+		else if(square>=32 && square<40)
+			row= 4;
+		else if(square>=40 && square<48)
+			row= 5;
+		else if(square>=48 && square<56)
+			row= 6;
+		else
+			row= 7;
+		if((row+col)%2==0)
+			return false;
 		System.out.println(square);
 		return true;
 	}
--- EightQueens_Question/src/GameStrategy.java
+++ EightQueens_Question/src/GameStrategy.java
@@ -2,16 +2,27 @@ public class GameStrategy {
 	private boolean placedQueens[][] = new boolean[8][8];
 	private int numQueens = 0;
 
-	private int getColumn(int cellId) {
-		// WRITE YOUR LOGIC HERE...................................		
-
-		return 0;	// just for the heck of it
+	private int getColumn(int cellId) {	
+		return (cellId%8);
 	}
 	
 	private int getRow(int cellId) {
-		// WRITE YOUR LOGIC HERE....................................
-		
-		return 0;	// just for the heck of it
+		if(cellId<8)
+			return 0;
+		else if(cellId>=8 && cellId<16)
+			return 1;
+		else if(cellId>=16 && cellId<24)
+			return 2;
+		else if(cellId>=24 && cellId<32)
+			return 3;
+		else if(cellId>=32 && cellId<40)
+			return 4;
+		else if(cellId>=40 && cellId<48)
+			return 5;
+		else if(cellId>=48 && cellId<56)
+			return 6;
+		else
+			return 7;
 	}
 
 	public boolean isValidPosition(int cellId) {
@@ -22,13 +33,23 @@ public class GameStrategy {
 		// the game is still on, so let us continue...
 		
 		int col = getColumn(cellId);
-		
+		int i,j;
 		int row = getRow(cellId);
 		
-		/*
-			WRITE YOUR LOGIC HERE...............................
-
-		*/
+		for(i=0;i<col;i++)
+			if(placedQueens[row][i]==true)
+				isValid = false;
+		for(i=row,j=col;i>=0 && j>=0;i--,j--)
+			if(placedQueens[i][j]==true)
+				isValid = false;
+		for(i=row,j=col;j>=0 && i<64;i++,j--)
+			if(placedQueens[i][j]==true)
+				isValid=false;
+		if(isValid==true)
+		{
+			placedQueens[row][col]=true;
+			numQueens++;
+		}
 		return isValid;
 	}
 }
